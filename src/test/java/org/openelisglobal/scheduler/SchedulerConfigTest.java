78e993486a25f37095b669b82f654f62c76716bb
Will Ponczak <willponczak@gmail.com>
2025-04-01 11:43:15 -0400

    ran mvn spotlesss:apply and removed comments

--- src/test/java/org/openelisglobal/scheduler/SchedulerConfigTest.java
+++ src/test/java/org/openelisglobal/scheduler/SchedulerConfigTest.java
@@ -1,65 +1,41 @@
 package org.openelisglobal.scheduler;
 
-import org.junit.Before;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Mockito.*;
-import org.mockito.MockitoAnnotations;
 import org.mockito.MockitoAnnotations.*;
-import org.mockito.Mock;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-
-import javax.swing.Spring;
-
-import org.openelisglobal.scheduler.service.CronSchedulerService;
-import org.quartz.Scheduler;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.openelisglobal.common.util.ConfigurationProperties;
-import org.openelisglobal.common.util.ConfigurationProperties.Property;
 import org.openelisglobal.common.util.DefaultConfigurationProperties;
-import org.openelisglobal.scheduler.SchedulerConfig;
-import org.openelisglobal.spring.util.SpringContext;
-import java.lang.reflect.Field;
-
-
-
+import org.openelisglobal.scheduler.service.CronSchedulerService;
 
 public class SchedulerConfigTest {
 
     @InjectMocks
     private SchedulerConfig schedulerConfig;
-    
+
     @Mock
     private CronSchedulerService chronScheduleService;
 
-    @Mock 
+    @Mock
     private ConfigurationProperties configProperties;
 
     @Mock
     private DefaultConfigurationProperties configPropertiesMock;
 
-    // Create a method to access ConfigurationProperties.getInstance()
     private ConfigurationProperties originalConfigProperties;
-    
-   @Test
-   public void getResultsResendTimeMillisTestDefaultValue() {
-    long default_period_millis = 30L * 1000 * 60;
 
-    // Mock the behavior of getPropertyValue to return a valid value
-    Mockito.when(configProperties.getPropertyValue(Property.resultsResendTime)).thenReturn(null);
+    @Test
+    public void getResultsResendTimeMillisTestDefaultValue() {
+        long default_period_millis = 30L * 1000 * 60;
 
-    SchedulerConfig schedulerConfig = new SchedulerConfig();
+        SchedulerConfig schedulerConfig = new SchedulerConfig();
 
-    long result = schedulerConfig.getResultsResendTimeMillis();
+        long result = schedulerConfig.getResultsResendTimeMillis();
 
-    assertEquals(default_period_millis, result);
-   }
+        assertEquals(default_period_millis, result);
+    }
 
 }
